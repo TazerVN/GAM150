03c2bf5baebbc5f460cae45c7efd31dc862b4413
TazerVN <tuan.phminh@gmail.com>
2026-03-21 22:55:52 +0800

    T_Update: update Victory Card Selection Screen

--- source/UI/Victory_Select.h
+++ source/UI/Victory_Select.h
@@ -5,6 +5,7 @@ private:
 	Entity create_Victory_card(std::string cardname, f32 x, f32 y);
 public:
 	std::vector<Entity>victory_cards;
+	Entity dim;
 	bool on = false;
 	void update();
 	void free();
